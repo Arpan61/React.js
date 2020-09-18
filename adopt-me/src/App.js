220c2a79375ead61946afbc7fb89004cacc3dece
Arpan Patel <arpanvpatel61@gmail.com>
2020-09-18 16:38:48 -0700

    Crated SearchParams file

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,33 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
-import Pet from './pet'
+import SearchParams from './SearchParams'
 
 const App = () => {
-  // return React.createElement('div', { id: 'something-important' }, [
-  //   React.createElement('h1', {}, 'Adopt Me!'),
-  //   React.createElement(pet, {
-  //     name: 'Gabbar',
-  //     animal: 'Dog',
-  //     breed: 'German Shephard',
-  //   }),
-  //   React.createElement(pet, {
-  //     name: 'Doing',
-  //     animal: 'cat',
-  //     breed: 'mixed',
-  //   }),
-  //   React.createElement(pet, {
-  //     name: 'Toing',
-  //     animal: 'Pig',
-  //     breed: 'mixed',
-  //   }),
-  // ])
-
   return (
     <div id="something-important">
       <h1>Adopt Me!</h1>
-      <Pet name="gabbar" animal="Dog" breed="German Shephard" />
-      <Pet name="Doing" animal="cat" breed="mixed" />
-      <Pet name="Toing" animal="pig" breed="mixed" />
+      <SearchParams />
     </div>
   )
 }
