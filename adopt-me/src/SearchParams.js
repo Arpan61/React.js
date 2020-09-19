8ed865eb9d2d73018397a475c9d0401fbff934a7
Arpan Patel <arpanvpatel61@gmail.com>
2020-09-19 14:52:22 -0700

    Custom Hooks

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react'
-import { ANIMALS } from '@frontendmasters/pet'
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
-  const [location, setLocation] = useState('Seattle, WA')
-  const [animal, setAnimal] = useState('dog')
-  const [breed, setBreed] = useState('')
-  const [breeds, setBreeds] = useState([])
+  const [location, setLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
 
   return (
     <div classsName="search-params">
@@ -20,46 +21,12 @@ const SearchParams = () => {
           />
         </label>
 
-        <label htmlFor="animal">
-          Animal
-          <select
-            id="animal"
-            value={animal}
-            onChange={(e) => setAnimal(e.target.value)}
-            onBlur={(e) => setAnimal(e.target.value)}
-          >
-            <option>All</option>
-            {ANIMALS.map((animal) => {
-              return (
-                <option key={animal} value={animal}>
-                  {animal}
-                </option>
-              )
-            })}
-          </select>
-        </label>
-
-        <label htmlFor="breed">
-          Breed
-          <select
-            id="breed"
-            value={breed}
-            onChange={(e) => setBreed(e.target.value)}
-            onBlur={(e) => setBreed(e.target.value)}
-            disabled={breeds.length === 0}
-          >
-            <option>All</option>
-            {breeds.map((breedString) => (
-              <option key={breedString} value={breedString}>
-                {breedString}
-              </option>
-            ))}
-          </select>
-        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
         <button>Submit</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default SearchParams
+export default SearchParams;
