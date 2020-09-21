f36cd713357bbad3395292304db81de944d1258c
Arpan Patel <arpanvpatel61@gmail.com>
2020-09-20 17:06:32 -0700

    Effects

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -1,12 +1,24 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  useEffect(() => {
+    // pet.breeds("dog").then(console.log, console.error);
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(({ breeds: apibreeds }) => {
+      const breedString = apibreeds.map(({ name }) => name);
+      setBreeds(breedString);
+    }),
+      (error) => console.error(error);
+  }, [animal, setBreeds, setBreed]);
 
   return (
     <div classsName="search-params">
