46462cbc998be689c9b73ab18f18020a6527ae64
Arpan Patel <arpanvpatel61@gmail.com>
2020-09-22 16:14:19 -0700

    Update Pet Component

--- adopt-me/src/pet.js
+++ adopt-me/src/pet.js
@@ -1,17 +1,19 @@
-import React from 'react'
-export default function pet({ name, animal, breed }) {
-  //   return React.createElement('div', {}, [
-  //     React.createElement('h1', {}, name),
-  //     React.createElement('h2', {}, animal),
-  //     React.createElement('h2', {}, breed),
-  //   ])
-  // }
+import React from "react";
+export default function pet({ name, animal, breed, media, location, id }) {
+  let hero = "http://placecorgi.com/300/300";
+  if (media.length) {
+    hero = media[0].small;
+  }
 
   return (
-    <div>
-      <h1>Name:{name.toUpperCase()}</h1>
-      <h2>{animal}</h2>
-      <h2>{breed}</h2>
-    </div>
-  )
+    <a href={`\details\${id}`} calssname="pet">
+      <div className="image-container">
+        <img src={hero} alt={name}></img>
+      </div>
+      <div calssName="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+      </div>
+    </a>
+  );
 }
