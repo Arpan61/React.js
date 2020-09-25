70b8c40166e77df0417d68bda2411873a28bdbcc
Arpan Patel <arpanvpatel61@gmail.com>
2020-09-25 12:24:39 -0700

    Error Page redirects

--- adopt-me/src/Details.js
+++ adopt-me/src/Details.js
@@ -1,6 +1,7 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 // const Details = (props) => {
 //   return (
 //     <pre>
@@ -17,6 +18,7 @@ class Details extends React.Component {
   state = { loading: true };
 
   componentDidMount() {
+    // throw new Error("lol");
     pet
       .animal(this.props.id)
       .then(({ animal }) => {
@@ -53,4 +55,10 @@ class Details extends React.Component {
   }
 }
 
-export default Details;
+export default function DetailsWithErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
